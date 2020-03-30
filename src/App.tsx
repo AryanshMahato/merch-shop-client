a9d29e60e52f4cd31faa86eb711bc53f49e7d366
Aryansh Mahato <aryanshmahato@gmail.com>
2020-03-30 12:58:19 +0530

    Project migrated to Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Routes from "./Routes";
 import Navbar from "./Containers/Navbar/Navbar";
+import { Button } from "@material-ui/core";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Button>Let's get Started</Button>
       <Routes />
     </div>
   );
