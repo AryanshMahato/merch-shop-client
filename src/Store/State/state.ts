4c442e12cd099cb14ef8d8070e30a951f45d83c7
Aryansh Mahato <aryanshmahato@gmail.com>
2020-03-31 22:58:32 +0530

    Redux Dev Tools Added

--- src/Store/State/state.ts
+++ src/Store/State/state.ts
@@ -1,7 +1,10 @@
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
 import reducer from "../Reducer/Product";
 import thunk from "redux-thunk";
 
-const state = createStore(reducer, applyMiddleware(thunk));
+const composeEnhancers =
+  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const state = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 export default state;
