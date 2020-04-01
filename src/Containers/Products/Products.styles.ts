5ce8704463db9303b60728d75faa3dc30b6be347
Aryansh Mahato <aryanshmahato@gmail.com>
2020-04-01 19:17:11 +0530

    Product Cards are now responsive

--- src/Containers/Products/Products.styles.ts
+++ src/Containers/Products/Products.styles.ts
@@ -1,11 +1,16 @@
 import { makeStyles } from "@material-ui/core";
+import { Theme } from "@material-ui/core/styles";
 
-const styles = makeStyles(() => ({
+const styles = makeStyles((theme: Theme) => ({
   product: {
     display: "grid",
-    gridTemplateColumns: "repeat(auto-fill,minmax(250px, 1fr))",
+    gridTemplateColumns: "repeat(auto-fill, minmax(250px, 1fr))",
     gridGap: "2rem",
-    padding: "1rem"
+    padding: "1rem",
+    justifyContent: "center",
+    [theme.breakpoints.down("xs")]: {
+      gridTemplateColumns: "150px 150px"
+    }
   }
 }));
 
