1a5cb856ff3865b4b4c24cdc1f4432d328e34e81
Aryansh Mahato <aryanshmahato@gmail.com>
2020-04-01 19:19:48 +0530

    Logo is now Responsive

--- src/Components/NavItems/Logo/Logo.styles.ts
+++ src/Components/NavItems/Logo/Logo.styles.ts
@@ -1,10 +1,16 @@
 import { makeStyles } from "@material-ui/core";
+import { Theme } from "@material-ui/core/styles";
 
-const styles = makeStyles(() => ({
-  logo: {
+const styles = makeStyles((theme: Theme) => ({
+  root: {
     display: "flex",
     alignItems: "center",
     justifyContent: "center"
+  },
+  logo: {
+    [theme.breakpoints.down("xs")]: {
+      width: 250
+    }
   }
 }));
 
