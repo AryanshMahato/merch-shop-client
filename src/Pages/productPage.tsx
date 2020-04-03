0e31549943b2baedb9a68138ef76725ab7d5e9a9
Aryansh Mahato <aryanshmahato@gmail.com>
2020-04-03 20:08:18 +0530

    Product Center Bug Fixed on Big Screen

--- src/Pages/productPage.tsx
+++ src/Pages/productPage.tsx
@@ -1,20 +1,11 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 import ProductBackground from "../Components/ProductBackground/ProductBackground";
-import { makeStyles } from "@material-ui/core";
 import Product from "../Containers/Product/Product";
 
-const styles = makeStyles(() => ({
-  background: {
-    display: "flex",
-    justifyContent: "space-between"
-  }
-}));
-
 const ProductPage = ({ match }: any) => {
-  const classes = styles();
   return (
-    <ProductBackground className={classes.background}>
+    <ProductBackground>
       <Product id={match.params.id} />
     </ProductBackground>
   );
