99c66a275a53e37dc20ed8beaaeef234435650a7
Aryansh Mahato <aryanshmahato@gmail.com>
2020-04-03 22:28:44 +0530

    Formik material Ui Integration Implemented

--- src/Containers/Forms/SignInForm/SignInForm.tsx
+++ src/Containers/Forms/SignInForm/SignInForm.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Formik, Field, Form } from "formik";
 import { LoginButton } from "../../../Global/Button/Buttons";
 import signInSchema from "../../../Schema/signInSchema";
+import { TextField } from "formik-material-ui";
 
 const SignInForm = ({ formSubmit }: FormProps) => {
   return (
@@ -16,14 +17,21 @@ const SignInForm = ({ formSubmit }: FormProps) => {
         console.log(values);
       }}
     >
-      {({ errors, touched }) => (
+      {() => (
         <Form>
-          <Field name={"email"} />
-          {errors.email && touched.email ? <div>{errors.email}</div> : null}
-          <Field name={"password"} />
-          {errors.password && touched.password ? (
-            <div>{errors.password}</div>
-          ) : null}
+          <Field
+            name={"email"}
+            component={TextField}
+            variant={"outlined"}
+            label={"Email"}
+          />
+          <Field
+            name={"password"}
+            component={TextField}
+            variant={"outlined"}
+            label={"Password"}
+            type={"password"}
+          />
           <LoginButton />
         </Form>
       )}
