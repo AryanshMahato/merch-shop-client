bda919c99883b14cc1cc13e4a264cb64c65c4cf5
Aryansh Mahato <aryanshmahato@gmail.com>
2020-04-14 21:00:25 +0530

    Loading Screen Products bug fixed

--- src/Store/Action/Product.ts
+++ src/Store/Action/Product.ts
@@ -15,6 +15,7 @@ export function getProducts() {
         error: "No Product Found"
       });
     } else {
+      dispatch({ type: ActionTypes.IS_LOADING, isLoading: false });
       dispatch({ type: ActionTypes.FETCH_PRODUCTS, products });
     }
   };
