47b34b3694944bdc2de10ac3d641f43dc5eb3cce
Aryansh Mahato <aryanshmahato@gmail.com>
2020-04-15 22:05:58 +0530

    New Category Design Created

--- src/Components/NewCategory/NewCategory.tsx
+++ src/Components/NewCategory/NewCategory.tsx
@@ -1,10 +1,21 @@
 import React from "react";
-import { Modal } from "@material-ui/core";
+import { Modal, TextField } from "@material-ui/core";
+import styles from "./NewCategory.styles";
+import { SaveButton } from "../../Global/Button/Buttons";
 
 const NewCategory = ({ show, handleClose }: NewCategoryProps) => {
+  const classes = styles();
+
   return (
-    <Modal open={show} onClose={handleClose}>
-      <h1>Hello</h1>
+    <Modal open={show} onClose={handleClose} className={classes.root}>
+      <div className={classes.paper}>
+        <TextField
+          label={"Category"}
+          variant={"outlined"}
+          className={classes.input}
+        />
+        <SaveButton />
+      </div>
     </Modal>
   );
 };
