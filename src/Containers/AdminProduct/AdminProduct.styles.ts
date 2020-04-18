52caf003194bf09d1cf4a0b50bf4a558a0c0b53c
Aryansh Mahato <aryanshmahato@gmail.com>
2020-04-18 19:17:14 +0530

    Product Margin and Padding Fixed

---
+++ src/Containers/AdminProduct/AdminProduct.styles.ts
@@ -0,0 +1,12 @@
+import { makeStyles } from "@material-ui/core";
+
+const styles = makeStyles(() => ({
+  root: {
+    width: "100%"
+  },
+  annotation: {
+    width: "100%"
+  }
+}));
+
+export default styles;
--- src/Containers/AdminProduct/AdminProduct.tsx
+++ src/Containers/AdminProduct/AdminProduct.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import AdminProductAnnotation from "../../Components/AdminProduct/AdminProductAnnotation/AdminProductAnnotation";
 import AdminProductList from "../../Components/AdminProduct/AdminProductList/AdminProductList";
 import NewProduct from "../../Components/AdminProduct/NewProduct/NewProduct";
+import styles from "./AdminProduct.styles";
 
 const AdminProduct = () => {
+  const classes = styles();
   return (
-    <div>
-      <div>
+    <div className={classes.root}>
+      <div className={classes.annotation}>
         <AdminProductAnnotation />
       </div>
       <AdminProductList />
