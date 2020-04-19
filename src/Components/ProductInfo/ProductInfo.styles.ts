ada1e626d3e3e61e3bfa2a2c1b153208840ee62b
Aryansh Mahato <aryanshmahato@gmail.com>
2020-04-19 23:40:59 +0530

    Few Minor UI bug fixed in Product

--- src/Components/ProductInfo/ProductInfo.styles.ts
+++ src/Components/ProductInfo/ProductInfo.styles.ts
@@ -1,19 +1,21 @@
 import { makeStyles } from "@material-ui/core";
 
 const styles = makeStyles(() => ({
-  root: {},
+  root: {
+    whiteSpace: "pre-line",
+  },
   name: {
     fontSize: "2.5rem",
-    fontWeight: "bold"
+    fontWeight: "bold",
   },
   priceSign: { fontWeight: "bold" },
   price: { fontSize: "1.5rem", margin: "1rem 0" },
   category: {
-    margin: "1rem 0"
+    margin: "1rem 0",
   },
   description: {
-    marginTop: "2rem"
-  }
+    margin: "2rem 0",
+  },
 }));
 
 export default styles;
--- src/Containers/Product/Product.styles.ts
+++ src/Containers/Product/Product.styles.ts
@@ -1,4 +1,7 @@
-import { makeStyles, Theme } from "@material-ui/core";
+import {
+  makeStyles,
+  Theme,
+} from "@material-ui/core";
 
 const styles = makeStyles((theme: Theme) => ({
   root: {
@@ -8,20 +11,21 @@ const styles = makeStyles((theme: Theme) => ({
     marginTop: "5rem",
     width: "100%",
     maxWidth: 1000,
+    marginBottom: "2rem",
     [theme.breakpoints.down("sm")]: {
       flexFlow: "column",
       alignItems: "center",
-      marginTop: "2rem"
-    }
+      marginTop: "2rem",
+    },
   },
   leftPart: {
     width: "90vw",
     maxWidth: 500,
     [theme.breakpoints.down("sm")]: {
-      marginBottom: "3rem"
-    }
+      marginBottom: "3rem",
+    },
   },
-  rightPart: { width: "90vw", maxWidth: 300 }
+  rightPart: { width: "90vw", maxWidth: 300 },
 }));
 
 export default styles;
