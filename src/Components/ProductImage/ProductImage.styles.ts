dcecaee887cfb5b02da7eb4937c5f72fe942aa80
Aryansh Mahato <aryanshmahato@gmail.com>
2020-04-21 19:33:45 +0530

    Product Image bug fixed

--- src/Components/ProductImage/ProductImage.styles.ts
+++ src/Components/ProductImage/ProductImage.styles.ts
@@ -2,13 +2,14 @@ import { makeStyles } from "@material-ui/core";
 
 const styles = makeStyles(() => ({
   root: {
-    width: "90vw",
-    maxWidth: 500,
-    maxHeight: 500,
+    width: "100%",
     marginBottom: "4rem",
+    display: "flex",
+    justifyContent: "center",
   },
   image: {
-    width: "100%",
+    maxWidth: 300,
+    maxHeight: 300,
   },
 }));
 
